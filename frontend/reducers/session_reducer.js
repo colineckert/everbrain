0b9e041b1dd6d0a92c48b58ba6f74d80cb1d10a3
colineckert <colineckert10@gmail.com>
2021-02-08 14:39:56 -0800

    Update id for session state

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -4,14 +4,14 @@ import {
  } from '../actions/session_actions';
 
 const _nullSession = {
-  id: null
+  currentUserId: null
 }
 
 const sessionReducer = (oldState = _nullSession, action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return Object.assign({}, { id: action.user.id });
+      return Object.assign({}, { currentUserId: action.user.id });
     case LOGOUT_CURRENT_USER:
       return _nullSession;
     default:
