e996ecb58fb8766b127bbdb800eb5b1788eba441
colineckert <colineckert10@gmail.com>
2021-02-09 23:23:17 -0800

    Style nav bar login and demo links

--- frontend/components/home/home_nav.jsx
+++ frontend/components/home/home_nav.jsx
@@ -10,10 +10,18 @@ const HomeNav = () => {
           <img src={window.EnNavLogo} alt="Evernote Logo"/>
         </Link>
       </div>
+
+      {/* <div className="social-media-list">
+        <a href="https://github.com/colineckert/everbrain" target="_blank"><i className="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/colin-eckert/" target="_blank"><i className="fab fa-linkedin-in"></i></a>
+        <a href="https://angel.co/u/colin-eckert" target="_blank"><i className="fab fa-angellist"></i></a>
+      </div> */}
+
       <div className="home-nav-buttons">
-        <Link to="/login">Log in</Link>
-        <p>or</p>
-        <DemoLogin />
+        <Link className="home-nav-login" to="/login">Log in</Link>
+        <div id="home-nav-demo">
+          <DemoLogin />
+        </div>
       </div>
     </div>
   )
