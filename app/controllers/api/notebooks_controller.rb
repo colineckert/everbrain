0ce0bd84c0d39161b630947c448428d92d88c82a
colineckert <colineckert10@gmail.com>
2021-02-10 16:34:44 -0800

    Finish notebooks controller

--- app/controllers/api/notebooks_controller.rb
+++ app/controllers/api/notebooks_controller.rb
@@ -8,26 +8,41 @@ class Api::NotebooksController < ApplicationController
 
   def show
     @notebook = Notebook.find_by(id: params[:id])
-
-    if @notebook
-      render :show
-    else
-      render json: ["Notebook does not exit"], status: 422
-    end
+    render :show
   end
 
   def update
+    @notebook = Notebook.find_by(id: params[:id])
+
+    if @notebook.update(notebook_params)
+      render :show
+    else
+      render json: @notebook.errors.full_messages, status: 401
+    end
   end
 
   def create
+    @notebook = Notebook.new(notebook_params)
+
+    if @notebook.save
+      render :show
+    else
+      render json: @notebook.errors.full_messages, status: 401
+    end
   end
 
   def destroy
+    @notebook = current_user.notebooks.find_by(id: params[:id])
+    if @notebook && @notebook.destroy
+      render :index
+    else
+      render json: ['Notebook does not exist']
+    end
   end
 
   private
 
   def notebook_params
-    params.require(:notebook).permit(:name)
+    params.require(:notebook).permit(:name, :author_id)
   end
 end
