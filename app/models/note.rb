da2a45de3db988d9967d95b5f8a2a9c68f55901d
colineckert <colineckert10@gmail.com>
2021-03-08 11:16:26 -0800

    Add note and tags associations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,15 @@
 class Note < ApplicationRecord
-  # validates :body, presence: true
+  # validates :body, null: false
 
   belongs_to :notebook,
     foreign_key: :notebook_id,
     class_name: :Notebook
+
+  has_many :note_tags,
+    foreign_key: :note_id,
+    class_name: :NoteTag
+
+  has_many :tags, 
+    through: :note_tags,
+    source: :tag
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,11 @@
 class Tag < ApplicationRecord
+  validates :name, presence: true, uniqueness: { scope: :author_id }
+
+  has_many :note_tags,
+    foreign_key: :tag_id,
+    class_name: :NoteTag
+
+  has_many :notes,
+    through: :note_tags, 
+    source: :note
 end
