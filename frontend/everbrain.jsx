6c10e85c50ed7498c1ce7718095d78d948d75d3d
colineckert <colineckert10@gmail.com>
2021-03-30 10:05:03 -0700

    Remove testing comments

--- frontend/everbrain.jsx
+++ frontend/everbrain.jsx
@@ -4,20 +4,6 @@ import configureStore from "./store/store";
 import Root from "./components/root";
 import 'react-quill/dist/quill.snow.css';
 
-// import { login, logout } from './actions/session_actions';
-// import { 
-//   requestNotebooks, requestNotebook, createNotebook, 
-//   updateNotebook, deleteNotebook 
-// } from './actions/notebook_actions';
-// import {
-//   requestNotes, requestNote, createNote,
-//   updateNote, deleteNote 
-// } from './actions/note_actions'
-
-import { getAllNotebookNotes, getAllTags } from './reducers/selectors';
-import { createTag, requestTags, requestTag, deleteTag } from './actions/tag_actions';
-
-
 document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (window.currentUser) {
@@ -34,30 +20,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // // TESTING START
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  // window.login = login;
-  // window.logout = logout;
-  // // notebooks
-  // window.requestNotebooks = requestNotebooks;
-  // window.requestNotebook = requestNotebook;
-  // window.createNotebook = createNotebook;
-  // window.updateNotebook = updateNotebook;
-  // window.deleteNotebook = deleteNotebook;
-  // // notes
-  // window.requestNotes = requestNotes;
-  // window.requestNote = requestNote;
-  // window.createNote = createNote;
-  // window.updateNote = updateNote;
-  // window.deleteNote = deleteNote;
-  // tags
-  window.requestTags = requestTags;
-  window.requestTag = requestTag;
-  window.createTag = createTag;
-  window.deleteTag = deleteTag;
-  // // selectors
-  // window.getAllNotebookNotes = getAllNotebookNotes;
-  window.getAllTags = getAllTags;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
   // // TESTING END
 
   const root = document.getElementById("root");
